445bd051b3218a43866a6f894a40585f63d72bff
Robert Pedersen <rob84044@gmail.com>
2019-08-07 21:11:52 -0600

    Set up ActionsRouter.js

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const db = require('./data/dbConfig.js');
 
+const ActionsRouter = require('./routers/ActionsRouter');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/actions', ActionsRouter);
+
+
+server.get('/', (req, res) => {
+ res.send('<h3>Welcome to the webdb-i-challenge</h3>');
+});
+
 
 module.exports = server;
