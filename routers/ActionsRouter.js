0bd07adbccba66dec49b9fdcff96da59d6f29942
Robert Pedersen <rob84044@gmail.com>
2019-08-07 21:14:47 -0600

    Added the two GET requests for ActionsRouter

--- routers/ActionsRouter.js
+++ routers/ActionsRouter.js
@@ -19,4 +19,25 @@ router.get('/', async(req, res) => {
 });
 
 
+router.get('/:id', async(req, res) => {
+ const {id} =req.params
+  try{
+   // Shows the actual query made to the db
+   // const query =db.select('*').from('posts')
+   // console.log(query)
+   
+   const [post] = await db.select('*').from('posts').where({id});
+   if (post) {
+    res.status(200).json(post);
+   } else {
+    res.status(400).json({message: `Could not find post with id ${id}`})
+   }
+   
+  }catch (error){
+   res.status(500).json({message: "Could not get specific post", error:error})
+  }
+ 
+ });
+ 
+ 
 module.exports = router;
